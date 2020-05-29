9c6606c06464fd4b42ac69299534c51691dffe7b
giorgioGunawan <grggnwn@gmail.com>
2020-05-29 18:14:12 +1000

    Updates

---
+++ precache-manifest.f52d3996b141d4d0dceb09274f1449a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e00c75d1d176e815279b8672dade1fa",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "4dff51e14687c1d87227",
+    "url": "/movie_app/static/css/main.95334dbb.chunk.css"
+  },
+  {
+    "revision": "de88fe7d39cd092b322c",
+    "url": "/movie_app/static/js/2.8fc079b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.8fc079b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dff51e14687c1d87227",
+    "url": "/movie_app/static/js/main.124cecb6.chunk.js"
+  },
+  {
+    "revision": "793037b19926e1970ea2",
+    "url": "/movie_app/static/js/runtime-main.8469f341.js"
+  }
+]);
