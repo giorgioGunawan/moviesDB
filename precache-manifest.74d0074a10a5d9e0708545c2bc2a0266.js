8bc3bb583566b3bc76b32aaf85dee6607b35843d
giorgioGunawan <grggnwn@gmail.com>
2020-05-29 18:11:14 +1000

    Updates

---
+++ precache-manifest.74d0074a10a5d9e0708545c2bc2a0266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e39043bd0ff48ac645944d73362ad8e",
+    "url": "/giorgiogunawan.github.io/movie_app/index.html"
+  },
+  {
+    "revision": "324e3a17f6164ac294b2",
+    "url": "/giorgiogunawan.github.io/movie_app/static/css/main.95334dbb.chunk.css"
+  },
+  {
+    "revision": "de88fe7d39cd092b322c",
+    "url": "/giorgiogunawan.github.io/movie_app/static/js/2.8fc079b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giorgiogunawan.github.io/movie_app/static/js/2.8fc079b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324e3a17f6164ac294b2",
+    "url": "/giorgiogunawan.github.io/movie_app/static/js/main.9d931265.chunk.js"
+  },
+  {
+    "revision": "0f3c61fc4023898ea46e",
+    "url": "/giorgiogunawan.github.io/movie_app/static/js/runtime-main.fa9d932b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.f52d3996b141d4d0dceb09274f1449a4.js"
+  "/giorgiogunawan.github.io/movie_app/precache-manifest.74d0074a10a5d9e0708545c2bc2a0266.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/giorgiogunawan.github.io/movie_app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
